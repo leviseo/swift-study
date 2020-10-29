908d0468f656481cc0446be87e579a92a2c5077f
leviseo <seojuhee4@gmail.com>
2020-10-29 14:05:28 +0900

    ADD viewController init

---
+++ VCTest2/VCTest2/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  VCTest2
+//
+//  Created by Levi on 2020/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
